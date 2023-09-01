9c864ffad06518ecf0066fbfd4369a13d2b42a2e
faizan <sfh8881212@gmail.com>
2023-08-31 23:54:32 -0700

    done

---
+++ resources/views/contacts/index.blade.php
@@ -0,0 +1,55 @@
+@extends('contacts.layout')
+@section('content')
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-9">
+                <div class="card">
+                    <div class="card-header">Contacts</div>
+                    <div class="card-body">
+                        <a href="{{ url('/contact/create') }}" class="btn btn-success btn-sm" title="Add New Contact">
+                            <i class="fa fa-plus" aria-hidden="true"></i> Add New
+                        </a>
+                        <br/>
+                        <br/>
+                        <div class="table-responsive">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Name</th>
+                                        <th>Address</th>
+                                        <th>Telephone</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($contacts as $item)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $item->name }}</td>
+                                        <td>{{ $item->address }}</td>
+                                        <td>{{ $item->mobile }}</td>
+
+                                        <td>
+                                            <a href="{{ url('/contact/' . $item->id) }}" ><button class="btn btn-info btn-sm"> View</button></a>
+                                            <a href="{{ url('/contact/' . $item->id . '/edit') }}" ><button class="btn btn-primary btn-sm"> Edit</button></a>
+
+                                            <form method="POST" action="{{ url('/contact' . '/' . $item->id) }}"  style="display:inline">
+                                                {{ method_field('DELETE') }}
+                                                @csrf
+                                                <button type="submit" class="btn btn-danger btn-sm" title="Delete Contact" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
